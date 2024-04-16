fbb287d7a7fa322be3bb53a1b502285c6bb0fb05
Mika Iivari <mika.iivari@aiforia.com>
2024-04-16 08:19:43 +0300

    Upgraded ElasticSearch version to 7.17.20-SNAPSHOT, reverted previous commit due to runtime problems

--- server/src/main/java/org/elasticsearch/Version.java
+++ server/src/main/java/org/elasticsearch/Version.java
@@ -170,7 +170,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
 	public static final Version V_7_17_7 = new Version(7_17_07_99, org.apache.lucene.util.Version.LUCENE_8_11_1);
 	public static final Version V_7_17_18 = new Version(7_17_18_99, org.apache.lucene.util.Version.LUCENE_8_11_1);
 	public static final Version V_7_17_19 = new Version(7_17_19_99, org.apache.lucene.util.Version.LUCENE_8_11_1);
-    public static final Version CURRENT = V_7_17_19;
+	public static final Version V_7_17_20 = new Version(7_17_20_99, org.apache.lucene.util.Version.LUCENE_8_11_1);
+    public static final Version CURRENT = V_7_17_20;
 
     private static final ImmutableOpenIntMap<Version> idToVersion;
     private static final ImmutableOpenMap<String, Version> stringToVersion;
